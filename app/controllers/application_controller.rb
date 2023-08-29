05dec99fbba853867f31b7b062d024eecfa88f06
Atril33 <118503473+Atril33@users.noreply.github.com>
2023-08-29 15:29:07 +0500

    Add Devise Gem For Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user  
+  skip_before_action :verify_authenticity_token, only: [:create]
 
-  def current_user
-    @current_user ||= User.first
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(_resource)
+    users_path
+  end
+
+  def after_sending_reset_password_instructions_path_for(resource_name)
+    new_session_path(resource_name) if is_navigational_format?
   end
 end
